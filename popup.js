4d8a89adbccf091070ffe48337eb0efc4df1e4e6
kundan <kundankumar28884@gmail.com>
2024-10-01 16:45:55 +0530

    styling added, custom text translation added

--- popup.js
+++ popup.js
@@ -1,3 +1,42 @@
+// code for opening and closing custom tralation
+const writeTextDiv = document.getElementById("write-text");
+const openBtn = document.getElementById("open-btn");
+const closeBtn = document.getElementById("close-btn");
+
+openBtn.addEventListener("click", () => {
+  writeTextDiv.classList.remove("hidden");
+  writeTextDiv.classList.add("flex");
+});
+
+closeBtn.addEventListener("click", () => {
+  writeTextDiv.classList.remove("flex");
+  writeTextDiv.classList.add("hidden");
+});
+// --------------------------------
+
+// translate custom-text
+
+const translateBtn = document.getElementById("custom-translate");
+const textarea1 = document.getElementById("textarea1");
+const textarea2 = document.getElementById("textarea2");
+const selectTags = document.querySelectorAll("select");
+
+translateBtn.addEventListener("click", () => {
+  const translateFrom = selectTags[0].value;
+  const translateTo = selectTags[1].value;
+  // console.log(translateFrom, translateTo);
+  const content = textarea1.value;
+  let apiUrl = `https://api.mymemory.translated.net/get?q=${content}&langpair=${translateFrom}|${translateTo}`;
+  fetch(apiUrl).then((res) =>
+    res.json().then((data) => {
+      // console.log(data);
+      textarea2.value = data?.responseData.translatedText;
+    })
+  );
+});
+
+// ----------------
+
 const selectTag = document.querySelectorAll("select");
 selectTag.forEach((tag, id) => {
   for (const country_code in countries) {
@@ -12,13 +51,14 @@ selectTag.forEach((tag, id) => {
   }
 });
 
+// code for chat translate
 document.getElementById("translate").addEventListener("click", () => {
   // Access the select elements
   const selectTags = document.querySelectorAll("select");
 
   const translateFrom = selectTags[0].value;
   const translateTo = selectTags[1].value;
-  console.log(translateFrom, translateTo);
+  // console.log(translateFrom, translateTo);
 
   // Send a message to the content script to modify the content
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
@@ -29,3 +69,4 @@ document.getElementById("translate").addEventListener("click", () => {
     });
   });
 });
+// ------------------------------
